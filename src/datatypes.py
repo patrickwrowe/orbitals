c9ab5ba204832d826a2a7047baa18284c60c4462
Patrick Rowe <patrick.william.rowe@gmail.com>
2024-08-18 09:54:40 -0700

    plot orbitals correctly

--- src/datatypes.py
+++ src/datatypes.py
@@ -3,13 +3,22 @@ from __future__ import annotations
 import xarray as xr
 import numpy as np
 import attrs
+from src import electron_functions
 
 @attrs.define
 class ElectronDensityVolume:
     resolution: dict
+    r_max: int = 1
 
     def _normalize(self):
-        self.density.data = self.density.data / self.density.data.sum()
+        self.density.data = self.density.data / np.nansum(self.density.data)
+
+    def meshgrid_coords(self):
+        return np.meshgrid(
+            self.density.coords["x"],
+            self.density.coords["y"],
+            self.density.coords["z"],
+        )
 
 @attrs.define
 class RadialElectronDensity(ElectronDensityVolume):
@@ -27,7 +36,7 @@ class RadialElectronDensity(ElectronDensityVolume):
             ),
             dims = ["r", "theta", "phi"],
             coords={
-                "r": np.linspace(0, 1, self.resolution["r"]),
+                "r": np.linspace(0, self.r_max, self.resolution["r"]),
                 "theta": np.linspace(0, 2*np.pi, self.resolution["theta"]),
                 "phi": np.linspace(0, np.pi, self.resolution["phi"])
             },
@@ -37,11 +46,6 @@ class RadialElectronDensity(ElectronDensityVolume):
         )
         
         self._normalize()
-
-        
-
-
-
 
 @attrs.define
 class CartesianElectronDensity(ElectronDensityVolume):
@@ -53,19 +57,49 @@ class CartesianElectronDensity(ElectronDensityVolume):
     density = attrs.field(init=False)
 
     def __attrs_post_init__(self):
+        self.density = xr.DataArray(
+            data = np.ones(
+                (self.resolution["x"], self.resolution["y"], self.resolution["z"])
+            ),
+            dims = ["x", "y", "z"],
+            coords={
+                "x": np.linspace(-self.r_max, self.r_max, self.resolution["x"]),
+                "y": np.linspace(-self.r_max, self.r_max, self.resolution["y"]),
+                "z": np.linspace(-self.r_max, self.r_max, self.resolution["z"])
+            },
+            attrs = {
+                "resolution": self.resolution,
+            }
+        )
+        
         self._normalize()
 
-        self.density = xr.DataArray(
-        data = np.zeros(
-            (self.resolution["x"], self.resolution["y"], self.resolution["z"])
-        ),
-        dims = ["x", "y", "z"],
-        coords={
-            "x": np.linspace(0, 1, self.resolution["x"]),
-            "y": np.linspace(0, 1, self.resolution["y"]),
-            "z": np.linspace(0, 1, self.resolution["z"])
-        },
-        attrs = {
-            "resolution": self.resolution,
-        }
-    )
+
+    def eval_density(self, n: int, l: int, m: int):
+        xx, yy, zz = np.meshgrid(
+            self.density.coords["x"],
+            self.density.coords["y"],
+            self.density.coords["z"],
+        )
+
+        rr, tt, pp = electron_functions.convert_cartesian_to_radial(
+            xx, yy, zz
+        )
+
+        self.density.data = electron_functions.wavefunction(
+            n, l, m, rr, tt, pp
+        )
+
+        self._normalize()
+    
+    def get_clipped_density(self, threshold: float):
+
+        electron_density = np.absolute(self.density.data) ** 2
+
+        dens_range = np.nanmax(electron_density) - np.nanmin(electron_density)
+        abs_threshold = threshold * dens_range
+
+        print(dens_range)
+        print(abs_threshold)
+
+        return np.where(electron_density < abs_threshold, np.nan, electron_density)
--- src/electron_functions.py
+++ src/electron_functions.py
@@ -47,8 +47,27 @@ def convert_radial_to_cartesian(r, theta, phi):
     returns:
     tuple, (x, y, z) cartesian coordinates
     """
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
+    x = r * np.sin(phi) * np.cos(theta)
+    y = r * np.sin(phi) * np.sin(theta)
+    z = r * np.cos(phi)
 
     return x, y, z
+
+@np.vectorize
+def convert_cartesian_to_radial(x, y, z):
+    """
+    Converts cartesian coordinates to radial coordinates.
+
+    args:
+    x: float, x coordinate
+    y: float, y coordinate
+    z: float, z coordinate
+
+    returns:
+    tuple, (r, theta, phi) radial coordinates
+    """
+    r = np.sqrt(x**2 + y**2 + z**2)
+    theta = np.arctan2(y, x)
+    phi = np.arccos(z / r)
+
+    return r, theta, phi
