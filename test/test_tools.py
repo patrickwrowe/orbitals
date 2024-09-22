c7c5ffcd63c6e81310818899ed58e404035e3816
Patrick Rowe <patrick.william.rowe@gmail.com>
2024-09-21 21:18:06 -0700

    test validation of quantum numbers and sketch out necessaries for testing other tools.

---
+++ test/test_tools.py
@@ -0,0 +1,47 @@
+import pytest
+from orbitals import tools
+
+def test_validate_quantum_numbers():
+    assert tools.validate_quantum_numbers(1, 0, 0) == True # 1s
+    assert tools.validate_quantum_numbers(2, 0, 0) == True # 2s
+
+    # 2p
+    assert tools.validate_quantum_numbers(2, 1, 0) == True
+    assert tools.validate_quantum_numbers(2, 1, 1) == True
+    assert tools.validate_quantum_numbers(2, 1, -1) == True
+
+
+    # 3d    
+    assert tools.validate_quantum_numbers(3, 2, 2) == True
+    assert tools.validate_quantum_numbers(3, 2, -2) == True
+
+    # failures
+    with pytest.raises(AssertionError):
+        tools.validate_quantum_numbers(0, 0, 0) # n must be a positive integer
+    
+    with pytest.raises(AssertionError):
+        tools.validate_quantum_numbers(1, 2, 0) # l must be +ve int or 0, up to n-1
+    
+    with pytest.raises(AssertionError):
+        tools.validate_quantum_numbers(1, 0, 1) # m can be +l to -l
+
+    with pytest.raises(AssertionError):
+        tools.validate_quantum_numbers(1, 0, 0, 0.4) # spin is +/- 0.5
+    
+    with pytest.raises(AssertionError):
+        tools.validate_quantum_numbers(1, 0, 0, 1) # spin is +/- 0.5
+
+def test_abs_threshold_from_relative():
+    pass
+
+def test_interpolate_grid_function():
+    pass
+
+def test_clip_density():
+    pass
+
+def test_convert_cartesian_to_radial():
+    pass
+
+def test_convert_radial_to_cartesian():
+    pass
