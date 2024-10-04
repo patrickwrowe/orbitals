61b1c5b631217c9e1eacd70ccb8b20a9c7d9ba95
Patrick Rowe <patrick.william.rowe@gmail.com>
2024-10-04 09:43:15 -0700

    replace __attrs_post_init__ malarky with actual classmethods.

--- orbitals/datatypes.py
+++ orbitals/datatypes.py
@@ -11,10 +11,11 @@ from orbitals import tools
 
 @attrs.define
 class WavefunctionVolume:
-    resolution: dict
-    r_max: int = 1
 
-    wavefunction: xr.DataArray = attrs.field(init=False)
+    wavefunction: xr.DataArray
+
+    resolution: dict
+    r_max: int
 
     def _normalize(self):
         # Normalise so sum of elements is 1
@@ -37,6 +38,10 @@ class WavefunctionVolume:
     def get_dims(self):
         return self.wavefunction.dims
 
+    @classmethod
+    def new_1e_atomic_wavefunction(cls, resolution: dict, r_max: int,  n: int, l: int, m: int):
+        raise NotImplementedError()
+
 
 @attrs.define
 class RadialWavefunction(WavefunctionVolume):
@@ -51,38 +56,50 @@ class RadialWavefunction(WavefunctionVolume):
     wavefunction: xarray.DataArray, radial electron wavefunction
     """
 
-    # Radial wavefunction with coords r, phi, psi
-    resolution: dict
+    @classmethod
+    def new_1e_atomic_wavefunction(cls, resolution: dict, r_max: int,  n: int, l: int, m: int) -> RadialWavefunction:
 
-    wavefunction = attrs.field(init=False)
+        # Check that we've been provided with physically meaningful inputs
+        assert tools.validate_quantum_numbers(n, l, m)
+        assert resolution.keys() == RadialCoords
 
-    def __attrs_post_init__(self):
         # Radial wavefunction with coords r, phi, psi
-        self.wavefunction = xr.DataArray(
+        wavefunction = xr.DataArray(
             data=np.ones(
                 (
-                    self.resolution[RadialCoords.R],
-                    self.resolution[RadialCoords.THETA],
-                    self.resolution[RadialCoords.PHI],
+                    resolution[RadialCoords.R],
+                    resolution[RadialCoords.THETA],
+                    resolution[RadialCoords.PHI],
                 )
             ),
             dims=[RadialCoords.R, RadialCoords.THETA, RadialCoords.PHI],
             coords={
                 RadialCoords.R: np.linspace(
-                    0, self.r_max, self.resolution[RadialCoords.R]
+                    0, r_max, resolution[RadialCoords.R]
                 ),
                 RadialCoords.THETA: np.linspace(
-                    0, 2 * np.pi, self.resolution[RadialCoords.THETA]
+                    0, 2 * np.pi, resolution[RadialCoords.THETA]
                 ),
                 RadialCoords.PHI: np.linspace(
-                    0, np.pi, self.resolution[RadialCoords.PHI]
+                    0, np.pi, resolution[RadialCoords.PHI]
                 ),
             },
             attrs={
-                "resolution": self.resolution,
+                "resolution": resolution,
+                "n": n,
+                "l": l,
+                "m": m,
             },
         )
 
+        return cls(wavefunction=wavefunction, resolution=resolution, r_max=r_max)
+
+    def __attrs_post_init__(self):
+        self.eval_wavefunction(
+            self.wavefunction.attrs["n"],
+            self.wavefunction.attrs["l"],
+            self.wavefunction.attrs["m"],
+        )
         self._normalize()
 
     def eval_wavefunction(self, n: int, l: int, m: int):
@@ -118,34 +135,46 @@ class CartesianWavefunction(WavefunctionVolume):
     resolution: dict
 
     # Cartesian wavefunction with coords x, y, z
-    wavefunction = attrs.field(init=False)
+    # wavefunction = attrs.field(init=False)
 
-    def __attrs_post_init__(self):
-        self.wavefunction = xr.DataArray(
+    @classmethod
+    def new_1e_atomic_wavefunction(cls, resolution: dict, r_max: int,  n: int, l: int, m: int) -> CartesianWavefunction:
+        wavefunction = xr.DataArray(
             data=np.ones(
                 (
-                    self.resolution[CartesianCoords.X],
-                    self.resolution[CartesianCoords.Y],
-                    self.resolution[CartesianCoords.Z],
+                    resolution[CartesianCoords.X],
+                    resolution[CartesianCoords.Y],
+                    resolution[CartesianCoords.Z],
                 )
             ),
             dims=[CartesianCoords.X, CartesianCoords.Y, CartesianCoords.Z],
             coords={
                 CartesianCoords.X: np.linspace(
-                    -self.r_max, self.r_max, self.resolution[CartesianCoords.X]
+                    -r_max, r_max, resolution[CartesianCoords.X]
                 ),
                 CartesianCoords.Y: np.linspace(
-                    -self.r_max, self.r_max, self.resolution[CartesianCoords.Y]
+                    -r_max, r_max, resolution[CartesianCoords.Y]
                 ),
                 CartesianCoords.Z: np.linspace(
-                    -self.r_max, self.r_max, self.resolution[CartesianCoords.Z]
+                    -r_max, r_max, resolution[CartesianCoords.Z]
                 ),
             },
             attrs={
-                "resolution": self.resolution,
+                "resolution": resolution,
+                "n": n,
+                "l": l,
+                "m": m,
             },
         )
 
+        return cls(wavefunction=wavefunction, resolution=resolution, r_max=r_max)
+
+    def __attrs_post_init__(self):
+        self.eval_wavefunction(
+            self.wavefunction.attrs["n"],
+            self.wavefunction.attrs["l"],
+            self.wavefunction.attrs["m"],
+        )
         self._normalize()
 
     def eval_wavefunction(self, n: int, l: int, m: int):
@@ -159,7 +188,7 @@ class CartesianWavefunction(WavefunctionVolume):
             self.wavefunction.coords[CartesianCoords.Z],
         )
 
-        rr, tt, pp = electron_functions.convert_cartesian_to_radial(xx, yy, zz)
+        rr, tt, pp = tools.convert_cartesian_to_radial(xx, yy, zz)
 
         self.wavefunction.data = electron_functions.wavefunction(n, l, m, rr, tt, pp)
 
